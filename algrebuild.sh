d206ebcbf3d5146fa5a6cba853ff24183e8471d6
Sarachan, Brion D <sarachan@ge.com>
2019-04-22 16:18:50 -0400

    Changed name of image to be prefixed with thriveitcr, for using the THRIVE fork of this module.

--- algrebuild.sh
+++ algrebuild.sh
@@ -2,16 +2,12 @@
 # Copyright (c) General Electric Company, 2017.  All rights reserved.
 
 echo ""
-echo "Removing algorithm container."
-docker rm rt106_algorithmtemplate_1
-
-echo ""
 echo "Removing algorithm image."
-docker rmi rt106/rt106-algorithm-sdk
+docker rmi thriveitcr/rt106-algorithm-sdk
 
 echo ""
 echo "Building algorithm image."
-docker build -t rt106/rt106-algorithm-sdk --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy .
+docker build -t thriveitcr/rt106-algorithm-sdk --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy .
 
 echo ""
 docker images
